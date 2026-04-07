afdacbc2958ca9a36c7d1daf49641d747371d141
domi-04 <dominik.dumencic@gmail.com>
2026-04-07 19:02:32 +0200

    Instalacija Reacta te izrada hello world backend servera

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Hello, world!" });
+});
+
+const PORT = process.env.PORT || 5000; // Ako nema u .env PORT onda defaultaj na 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
